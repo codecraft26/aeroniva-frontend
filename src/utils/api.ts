6a3bc739e16cf7a6cf3b10661e5c87b64f3f0a5a
codecraft <gaman0221@gmail.com>
2025-07-15 19:22:45 +0530

    adding .env

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import { Report, Violation, KPIResponse, FilterOptions, Filters } from './types';
 
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE || 'http://localhost:8000/api';
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE || 'https://backend.otito.in/api';
 
 export async function uploadReport(file: File): Promise<any> {
   const formData = new FormData();
